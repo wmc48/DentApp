83dbad25b43e20b87464aa338e8c61330399e321
Łukasz Czajka <lukasz.czajka48@gmail.com>
2024-12-12 22:08:01 +0100

    transfer RegistrationStaffController to RegistrationController

--- src/main/java/com/booking_dent/dentApp/controller/RegistrationController.java
+++ src/main/java/com/booking_dent/dentApp/controller/RegistrationController.java
@@ -1,11 +1,11 @@
 package com.booking_dent.dentApp.controller;
-
-
 import com.booking_dent.dentApp.model.dto.AccountDTO;
+import com.booking_dent.dentApp.model.dto.EmployeeDTO;
 import com.booking_dent.dentApp.model.dto.PatientDTO;
 import com.booking_dent.dentApp.model.dto.UserDTO;
 import com.booking_dent.dentApp.security.UserEntity;
 import com.booking_dent.dentApp.security.UserService;
+import com.booking_dent.dentApp.service.EmployeeService;
 import com.booking_dent.dentApp.service.PatientService;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
@@ -14,18 +14,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
-@RequestMapping("/register")
 @AllArgsConstructor
 public class RegistrationController {
 
     private final UserService userService;
+    private final EmployeeService employeeService;
     private final PatientService patientService;
 
-    @GetMapping
+    @GetMapping("/register")
     public String showRegistrationForm(Model model) {
         AccountDTO accountDTO = new AccountDTO();
         accountDTO.setUserDTO(new UserDTO()); //ustawianie obiektu userDTO
@@ -35,7 +34,7 @@ public class RegistrationController {
     }
 
     @Transactional
-    @PostMapping("/create")
+    @PostMapping("/register/create")
     public String registerUser(
             @ModelAttribute("registerDTO") AccountDTO registerDTO,
             RedirectAttributes redirectAttributes
@@ -45,6 +44,33 @@ public class RegistrationController {
 
         UserEntity newUser = userService.createAccount(userDTO, 4);
         patientService.addPatient(patientDTO, newUser);
+
+        redirectAttributes.addFlashAttribute("message", "Rejestracja zakończona sukcesem. Zaloguj się.");
+        return "redirect:/index";
+    }
+
+    @GetMapping("/staffView/register")
+    public String showRegistrationFormEmployee(Model model) {
+        AccountDTO accountDTO = new AccountDTO();
+        accountDTO.setUserDTO(new UserDTO()); //ustawianie obiektu userDTO
+        accountDTO.setEmployeeDTO(new EmployeeDTO()); //ustawianie obiektu patientDTO
+        model.addAttribute("accountDTO", accountDTO);
+        return "/staffView/register";
+    }
+
+    @Transactional
+    @PostMapping("/staffView/register/create")
+    public String registerUserEmployee(
+            @ModelAttribute("registerDTO") AccountDTO registerDTO,
+            RedirectAttributes redirectAttributes
+    ) {
+        UserDTO userDTO = registerDTO.getUserDTO();
+        EmployeeDTO employeeDTO = registerDTO.getEmployeeDTO();
+        //pobiera wartość "1 ADMIN", "3 LEKARZ", "2 PERSONEL"
+        int roleId = userDTO.getRoleId();
+        UserEntity newUser = userService.createAccount(userDTO, roleId);
+        // Dodanie pracownika powiązanego z użytkownikiem
+        employeeService.addEmployee(employeeDTO, newUser);
 
         redirectAttributes.addFlashAttribute("message", "Rejestracja zakończona sukcesem. Zaloguj się.");
         return "redirect:/index";
